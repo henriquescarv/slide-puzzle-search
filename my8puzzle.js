19dafaad97a0ba47e068b33294e42e35250d93e2
Henrique Carvalho <henrique.scarvalho01@gmail.com>
2024-04-15 19:14:13 -0300

    update on addOpenNode verification

--- my8puzzle.js
+++ my8puzzle.js
@@ -1,13 +1,18 @@
 const solutionBoard = [1, 2, 3, 4, 5, 6, 7, 8, 0];
 const initialBoard = [7, 4, 2, 8, 1, 5, 6, 3, 0];
 
+[ 0, 1, 2, 
+  3, 4, 5, 
+  6, 7, 8
+];
+
 const showBoard = (board) => {
   console.log(`[${board[0]}, ${board[1]}, ${board[2]}]`);
   console.log(`[${board[3]}, ${board[4]}, ${board[5]}]`);
   console.log(`[${board[6]}, ${board[7]}, ${board[8]}]`);
 }
 
-const isFinalState = (initialBoard, solutionBoard) => solutionBoard === initialBoard;
+const isFinalState = (board, solutionBoard) => JSON.stringify(solutionBoard) === JSON.stringify(board); // função para verificar se o board recebido é igual ao board solução
 
 const moveToLeft = (board, position) => {
   const node = [...board];
@@ -79,7 +84,7 @@ const uniformCost = (board) => {
   const addOpenNode = ({ board, cost, parent }) => {
     const alreadyOpen = open.find(node => JSON.stringify(node.board) === JSON.stringify(board)); // verifica se esse estado já está na lista de abertos
     const alreadyVisited = visited.find(node => JSON.stringify(node.board) === JSON.stringify(board)); // verifica se esse estado já está na lista de visitados
-    const hasLessCost = !!alreadyOpen && !!alreadyVisited && cost < alreadyOpen.cost && cost < alreadyVisited.cost; // verifica se esse nodo tem menor custo do que o que já foi visitado
+    const hasLessCost = !!(alreadyOpen && cost < alreadyOpen.cost) || !!(alreadyVisited && cost < alreadyVisited.cost); // verifica se esse nodo tem menor custo do que o que já foi visitado
 
     const newNode = {
       board: board,
@@ -90,23 +95,28 @@ const uniformCost = (board) => {
       ],
     }
 
-    if (!alreadyOpen && !alreadyVisited) { // se o nodo ainda não tiver sido visitado, add na lista
+    if (!alreadyOpen && !alreadyVisited) { // se o nodo ainda não tiver sido visitado/aberto, add na lista
       open.push(newNode); // add o novo nodo na lista de abertos
       return;
     }
 
-    if (alreadyOpen && hasLessCost) {
-      open = open.filter(state => state !== alreadyOpen); // remove o que já foi visitado, pois tem maior custo
+    if ((alreadyOpen || alreadyVisited) && hasLessCost) { // se o nodo já tiver sido visitado/aberto, e tiver menor custo, add na lista e remove o(s) antigo(s)
+      if (!!alreadyOpen) {
+        open = open.filter(state => state !== alreadyOpen); // remove o que já foi aberto, pois tem maior custo
+      }
+      if (!!alreadyVisited) {
+        visited = visited.filter(state => state !== alreadyVisited); // remove o que já foi visitado, pois tem maior custo
+      }
       open.push(newNode);
     }
   }
 
   let currentState = board;
 
-  while (JSON.stringify(currentState) !== JSON.stringify(solutionBoard)) {
+  while (!isFinalState(currentState, solutionBoard)) {
+    open = open.sort((a, b) => a.cost - b.cost); // ordena a lista a partir do 'cost'
     const current = open[0]; // pega o nodo com menor custo
     currentState = current.board;
-    open = open.sort((a, b) => a.cost - b.cost); // ordena a lista a partir do 'cost'
 
     const newNodes = searchNodes(current.board); // procura novos nodos, a partir do 'current'
     newNodes.forEach((board, index) => addOpenNode({ board, cost: current.cost + (index + 1), parent: current }));
@@ -116,10 +126,7 @@ const uniformCost = (board) => {
 
   console.log(visited);
   console.log(open);
+  console.log('Fim de jogo!');
 };
 
 uniformCost(initialBoard);
-
-if (isFinalState(initialBoard, solutionBoard)) {
-  console.log('Fim de jogo!');
-}
